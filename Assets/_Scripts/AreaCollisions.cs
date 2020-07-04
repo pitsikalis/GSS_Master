e366f368121b59605feadaa5b7577cc4b7017605
pitsikalis <59825901+pitsikalis@users.noreply.github.com>
2020-07-04 20:09:22 +0100

    More stuff

--- Assets/_Scripts/AreaCollisions.cs
+++ Assets/_Scripts/AreaCollisions.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class AreaCollisions : MonoBehaviour
 {
-   
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    Debug.Log("Collision Enter");
-    //}
 
-    private void OnTriggerEnter2D(Collider2D collider)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Trigger Enter");
+        Debug.Log("Collision Enter");
     }
+
+    //private void OnTriggerEnter2D(Collider2D collider)
+    //{
+    //    Debug.Log("Trigger Enter");
+    //}
 }
