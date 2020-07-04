da45e36d3cc87d196c0eac8755985202b9d02059
pitsikalis <59825901+pitsikalis@users.noreply.github.com>
2020-07-04 20:06:12 +0100

    Collision Test

---
+++ Assets/_Scripts/AreaCollisions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaCollisions : MonoBehaviour
+{
+   
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log("Collision Enter");
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Trigger Enter");
+    }
+}
