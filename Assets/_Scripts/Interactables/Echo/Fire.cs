ca5e3363eab0c72aee30123da650329544cd8b05
pitsikalis <59825901+pitsikalis@users.noreply.github.com>
2020-08-17 03:11:13 +0100

    More stuff

---
+++ Assets/_Scripts/Interactables/Echo/Fire.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public LayerMask layerMask;
+
+
+    [SerializeField] private UnityEvent OnActivated;
+    [SerializeField] private UnityEvent OnDeactivated;
+
+ 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 12) // Shut down
+        {
+            OnDeactivated.Invoke();
+        }
+
+        if (other.gameObject.layer == 13) // Fire Grow
+        {
+            OnActivated.Invoke();
+        }
+
+    }
+}
